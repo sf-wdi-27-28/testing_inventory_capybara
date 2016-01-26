07b92c39b8925afd47208e8c0bc8d76f92b014e5
tgaff <tgaff@alumni.nd.edu>
2016-01-25 18:38:48 -0800

    add a seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+10.times do
+  wholesale_price = Random.new.rand(1.0..100.0).round(2)
+  name = FFaker::Lorem.words(5).join(" ")
+  description = FFaker::Lorem.sentence
+  category = FFaker::Lorem.words(3).join
+  sku = FFaker::Lorem.words(2).join
+  wholesale = wholesale_price
+  retail = wholesale_price * 4
+  new_product = Product.new(name: name, description: description, category: category, sku: sku, wholesale: wholesale, retail: retail)
+  3.times do
+    size = ['xs', 's', 'm', 'l', 'xl', 'xxl'].sample
+    color = FFaker::Color.name
+    status = [ 'available', 'in transit' ].sample
+    new_product.items << Item.create(size: size, color: color, status: status)
+  end
+  new_product.save
+end
